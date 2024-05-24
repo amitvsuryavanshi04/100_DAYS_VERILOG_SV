df2d58bf2f7ad37b16060b510c7e1a6e142c9347
AMIT SURYAVANSHI <108608256+amitvsuryavanshi04@users.noreply.github.com>
2024-05-24 08:41:26 +0530

    Create multiplexer_tb.v

---
+++ multiplexer_[day-04]/multiplexer_tb.v
@@ -0,0 +1,10 @@
+//test bench code for multiplexer with 8 inputs and 3 select lines 
+module multiplexer_tb();
+reg [2:0]sel;
+reg [7:0]a;
+wire muxout;
+multiplexer uut(.sel(sel),.a(a),.muxout(muxout));
+initial begin
+a=8'b00000000;
+#10;a=8'b00000001;sel=3'b000;
+#10;a=8'b00000010;sel=3'b001;
